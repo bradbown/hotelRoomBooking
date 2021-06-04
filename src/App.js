b9cd2543dc51a903ad1e13186c049bc459a6fd11
Bradley Bown <bradbown@googlemail.com>
2021-06-04 17:04:40 +0100

    project created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" color="secondary">Hello World</Button>
+        <img src={logo} className="App-logo" alt="logo" />        
+      </header>
+    </div>
+  );
+}
+
+export default App;
