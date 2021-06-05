c8ab9b6142fed3ee1dc84887fb54b6479a887c2c
Bradley Bown <bradbown@googlemail.com>
2021-06-05 12:01:52 +0100

    Messing around with material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField"
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="contained" color="secondary">Hello World</Button>
-        <img src={logo} className="App-logo" alt="logo" />        
+        <TextField 
+          variant="outlined"
+          style={{ color: "#61dafb" }}
+          type="date"
+        />
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button startIcon={<SaveIcon />} color="primary">
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            variant="contained"
+            style={{
+              background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+              color: "white",
+            }}
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
